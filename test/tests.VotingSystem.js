62b0e0ff6a2553bd9709429515b773dbee50f693
xyAshay <ashay.mailbox@gmail.com>
2019-12-11 20:53:45 +0530

    Added test case to ensure initial voteCount

--- test/tests.VotingSystem.js
+++ test/tests.VotingSystem.js
@@ -1,11 +1,32 @@
 const VotingSystem = artifacts.require("./VotingSystem.sol");
 
 contract("VotingSystem", () => {
+    var app;
+
     it("Initialize With Correct No Of Candidates", () => {
-        return VotingSystem.deployed().then((app) => {
+        return VotingSystem.deployed().then((instance) => {
+            app = instance;
             return app.candidateCount();
         }).then((count) => {
             assert.equal(count, 4);
         });
+    });
+
+    it("Initial VoteCount Must Be 0",() => {
+        return VotingSystem.deployed().then((instance) => {
+            app = instance;
+            return app.candidates(1);
+        }).then((temp) => {
+            assert.equal(temp[2],0);
+            return app.candidates(2);
+        }).then((temp) => {
+            assert.equal(temp[2],0);
+            return app.candidates(3);
+        }).then((temp) => {
+            assert.equal(temp[2],0);
+            return app.candidates(4);
+        }).then((temp) => {
+            assert.equal(temp[2],0);
+        })
     });
 });
